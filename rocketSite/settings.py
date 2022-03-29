30bd68e0d0a4aa496c0595f5612787e6c6864325
msstateACosby <92938995+msstateACosby@users.noreply.github.com>
2022-03-29 12:31:31 -0500

    Update settings.py

--- rocketSite/settings.py
+++ rocketSite/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%_q)k^7c06^q_eo)m#cip$lt+q2!)8ou3urq-urwgo0a8@udb'
+SECRET_KEY = ''
+
+with open("secretKeyFile.txt") as secret:
+    SECRET_KEY = secret.readline()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
